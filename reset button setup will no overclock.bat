971c675f34bfddc7694001eec7d3382f593df72b
ryderguy111 <ryde7546@gmail.com>
2026-03-15 13:27:51 -0600

    fixed reset button script
    Updated script to combine commands for making boot script into a single line.

--- reset button setup will no overclock.bat
+++ reset button setup will no overclock.bat
@@ -9,12 +9,9 @@ adb shell "echo '  getevent -lc 1 /dev/input/event2 | grep -q KEY_BACKSPACE && s
 adb shell "echo 'done' >> /data/local/tmp/launch.sh"
 
 echo Making boot script
-adb shell su -c "mount -o remount,rw /system"
-adb shell su -c "echo '#!/system/bin/sh' > /system/etc/install-recovery.sh"
-adb shell su -c "echo 'sh /data/local/tmp/launch.sh &' >> /system/etc/install-recovery.sh"
-adb shell su -c "chmod 755 /system/etc/install-recovery.sh"
+adb shell su -c "mount -o remount,rw /system && echo '#!/system/bin/sh' > /system/etc/install-recovery.sh && echo 'sh /data/local/tmp/launch.sh &' >> /system/etc/install-recovery.sh && chmod 755 /system/etc/install-recovery.sh"
 
-echo Starting script now...
+echo Starting script now
 adb shell "sh /data/local/tmp/launch.sh &"
 
 echo Hold reset for 2 seconds to open launcher
